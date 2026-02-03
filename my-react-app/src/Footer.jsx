d6329795c72e080fc01ee6ecd8e164bbc60d8824
Roberto Hernandez Cruz <240396@utxicotepec.edu.mx>
2026-02-02 22:09:35 -0600

    Add: Se añadieron 2 componentes nuevos conocidos como Footer y Seccion ambos ya añadidos a la app principal.

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <p className="footer-izquierda">© Tecnologías de la Información.</p>
+        <p className="footer-derecha">Marca Registrada®</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-react-app/src/Seccion.jsx
@@ -0,0 +1,18 @@
+import './Seccion.css';
+
+function Seccion() {
+  return (
+    <div className="seccion-container">
+      <div className="seccion-content">
+        <h1 className="seccion-titulo">Sección</h1>
+        <p className="seccion-texto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sint provident dolorum. 
+          Nisi asperiores velit eligendi minus reiciendis dignissimos voluptatibus odio doloremque 
+          nemo perspiciatis obcaecati, quia, assumenda voluptas labore voluptate?
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Seccion;
