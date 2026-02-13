f3bc09a7e785a18160c2c5eacec9fca9cfd6e6d4
Roberto Hernandez Cruz <240396@utxicotepec.edu.mx>
2026-02-13 14:10:24 -0600

    Add: Se añadioun archivo de geolocalizacion junto a su funcionabilidad

--- my-react-app/src/Seccion.jsx
+++ my-react-app/src/Seccion.jsx
@@ -1,4 +1,4 @@
-import Mapa from './mapa.jsx';
+import MapaGeolocalizacion from './mapaGeolocalizacion.jsx';
 import './Seccion.css';
 
 function Seccion() {
@@ -7,10 +7,7 @@ function Seccion() {
       <div className="seccion-content">
         <h1 className="seccion-titulo">Ztarbooks</h1>
         <p className="seccion-texto">Bienvenido a Ztarbooks, donde cada taza cuenta una historia de pasión por el café y la dedicación a la calidad. Desde nuestros granos seleccionados hasta el arte de preparar cada bebida, nos esforzamos por ofrecer una experiencia única que deleite tus sentidos. Ya sea que prefieras un espresso intenso, un latte cremoso o una infusión refrescante, en Starbucks encontrarás el sabor perfecto para cada momento del día. ¡Disfruta de tu café con nosotros!</p>
-          <Mapa 
-            lat={20.267235359887085}
-            lng={-97.741812}
-            nombre_sucursal={"Polanco"}/>
+          <MapaGeolocalizacion />
       </div>
     </div>
   );
